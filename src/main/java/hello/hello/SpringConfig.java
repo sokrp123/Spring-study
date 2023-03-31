ce86bfa500b9c66b649887292b31d8c5e6dba220
sokrp123 <sokrp123@naver.com>
2023-03-31 19:36:50 +0900

    message

---
+++ src/main/java/hello/hello/SpringConfig.java
@@ -0,0 +1,33 @@
+package hello.hello;
+
+import hello.hello.repository.MemberRepository;
+import hello.hello.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+   // @Bean
+   // public MemberRepository memberRepository(){
+        //return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        //return new JdbcTemplateMemberRepository(dataSource);
+        //return new JpaMemberRepository(em);
+
+   // }
+
+}
